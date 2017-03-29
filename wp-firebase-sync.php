4da4e57bcd128ebf9e8a769fd0633799c7bcba97
Jared Chu <me@jaredchu.com>
2017-03-29 18:54:20 +0700

    change app name

--- wp-firebase-sync.php
+++ wp-firebase-sync.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
-Plugin Name: Wp Firebase Sync
+Plugin Name: WP Firebase Sync
 Plugin URI: https://github.com/jaredchu/WP-FirebaseSync
 Description: Plugin that allow WordPress to sync Post data with Firebase in real time.
 Version: 1.0
-Author: jaredchu
+Author: Jared Chu
 Author URI: http://jaredchu.com/
 License: MIT
 */
