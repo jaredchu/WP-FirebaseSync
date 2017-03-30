a02110e130e607de8346db51e9e11692a73a850f
Jared Chu <me@jaredchu.com>
2017-03-30 10:41:40 +0700

    reformat classes

--- classes/Data.php
+++ classes/Data.php
@@ -12,31 +12,36 @@ require __DIR__ . '../vendor/autoload.php';
 use Defuse\Crypto\Crypto;
 use Defuse\Crypto\Key;
 
-class Data {
+class Data
+{
 
-	/**
-	 * @var Key
-	 */
-	private $encryptKey;
+    /**
+     * @var Key
+     */
+    private $encryptKey;
 
-	/**
-	 * Data constructor.
-	 *
-	 * @param Key $encryptKey
-	 */
-	public function __construct( Key $encryptKey ) {
-		$this->encryptKey = $encryptKey;
-	}
+    /**
+     * Data constructor.
+     *
+     * @param Key $encryptKey
+     */
+    public function __construct(Key $encryptKey)
+    {
+        $this->encryptKey = $encryptKey;
+    }
 
-	public static function fromKey( Key $key ) {
-		return new Data( $key );
-	}
+    public static function fromKey(Key $key)
+    {
+        return new Data($key);
+    }
 
-	public function encrypt( $plainText ) {
-		return Crypto::encrypt( $plainText, $this->encryptKey );
-	}
+    public function encrypt($plainText)
+    {
+        return Crypto::encrypt($plainText, $this->encryptKey);
+    }
 
-	public function decrypt( $plainText ) {
-		return Crypto::decrypt( $plainText, $this->encryptKey );
-	}
+    public function decrypt($plainText)
+    {
+        return Crypto::decrypt($plainText, $this->encryptKey);
+    }
 }
